f81f125a99800d54e3368ca4c44d12f93e14916b
morgan <morgan.smith@ephox.com>
2016-05-03 15:04:51 +1000

    running with server serving files

--- src/runner.js
+++ src/runner.js
@@ -2,6 +2,13 @@ var webdriver = require('selenium-webdriver'),
     chrome = require('selenium-webdriver/chrome'),
     firefox = require('selenium-webdriver/firefox'),
 
+    http=require("http"),
+    path=require('path'),
+    url=require('url'),
+
+    serveStatic=require('serve-static'),
+    finalhandler=require('finalhandler'),
+
     By = webdriver.By,
     until = webdriver.until,
     Condition = webdriver.Condition;
@@ -12,7 +19,38 @@ var webdriver = require('selenium-webdriver'),
     .setFirefoxOptions(/* ... */)
     .build();
 
-    driver.get('http://localhost/me/stash-morgan/bedrock/demo/index.html');
+    var serveProject = serveStatic('../../work/tbio/petrie');
+    var serveJs = serveStatic('../../work/tools/tunic/src/js');
+
+    http.createServer(function (request, response) {
+        var done = finalhandler(request, response);
+        var target = request.url;
+        if (target.indexOf('project/') > -1) {  
+            request.originalUrl = request.url;
+            request.url = request.url.substring('project/'.length);
+            serveProject(request, response, done);    
+        } else if (target.indexOf('js/') > -1) {
+            request.originalUrl = request.url;
+            request.url = request.url.substring('js/'.length);
+            serveJs(request, response, done);
+        } else {
+            response.writeHeader(200, {"Content-Type": "text/plain" });
+            response.write('Yo');
+            response.end();
+        }
+        
+
+        // console.log('response', response);
+        // var haha = url.parse(request.url).pathname;
+        // console.log('haha', haha);
+        // var full_path = path.join(process.cwd(),haha);
+        // console.log('full_path', full_path);
+        // response.writeHeader(200, {"Content-Type": "text/plain" });
+        // response.write('Yo');
+        // response.end();
+    }).listen(8080);
+
+    driver.get('http://localhost:8080/');
 
     var allStartTime = new Date().getTime();
     var testStartTime = new Date().getTime();
@@ -76,9 +114,9 @@ var webdriver = require('selenium-webdriver'),
     // driver.get('demo/index.html');
     // driver.findElement(By.name('q')).sendKeys('webdriver');
     // driver.findElement(By.name('btnG')).click();
-    driver.wait(nextTick, 1000).then(function (outcome) {
+    driver.wait(nextTick, 10).then(function (outcome) {
         outcome(driver);
     }, function () {
         allTestsTooLong(new Date().getTime() - allStartTime)();
     });
-    driver.quit();
+    // driver.quit();
