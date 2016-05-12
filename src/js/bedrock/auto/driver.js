29340f25716d09f57e3f06f5d51b8fee0bc6cef7
morgan <morgan.smith@ephox.com>
2016-05-12 11:24:02 +1000

    TINY-441: removing unused variables

--- src/js/bedrock/auto/driver.js
+++ src/js/bedrock/auto/driver.js
@@ -4,13 +4,9 @@
  */
 var create = function (settings) {
   var webdriver = require('selenium-webdriver');
-  var chrome = require('selenium-webdriver/chrome');
-  var firefox = require('selenium-webdriver/firefox');
 
   return new webdriver.Builder()
     .forBrowser(settings.browser)
-    .setChromeOptions(/* ... */)
-    .setFirefoxOptions(/* ... */)
     .build();
 };
 
