ffb1caea784baafc605bd3d7911c908a2d2dc130
morgan <morgan.smith@ephox.com>
2016-05-12 11:23:17 +1000

    TINY-441: unused variables

--- src/js/bedrock/remote/driver.js
+++ src/js/bedrock/remote/driver.js
@@ -1,15 +1,11 @@
 /* Settings:
  *
  * browser: the name of the browser
+ * os: the name of the operating system
+ * browserVersion: the version of the browser
  */
 var create = function (sauceUser, sauceKey, settings) {
   var webdriver = require('selenium-webdriver');
-  var chrome = require('selenium-webdriver/chrome');
-  var firefox = require('selenium-webdriver/firefox');
-
-  // OS: Mac 10.11, linux, Win7, Win10
-  // Browser names: 'microsoftedge', 'chrome', 'safari', 'firefox', 'iexplore'
-  // Version: latest
 
   return new webdriver.Builder()
     .withCapabilities({
