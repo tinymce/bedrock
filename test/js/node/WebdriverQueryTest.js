2400f74cc7b3de6d5818009c234e2c6fa25c27fe
morgan <morgan.smith@ephox.com>
2016-06-06 10:31:26 +1000

    TINY-491-deadlocks: running interleaving requests

--- test/js/node/WebdriverQueryTest.js
+++ test/js/node/WebdriverQueryTest.js
@@ -1,22 +1,27 @@
 var driver = require('../../../src/js/bedrock/auto/driver').create({
-  browser: 'firefox'
+  browser: 'chrome'
 });
 
+driver.manage().timeouts().setScriptTimeout(30000);
 
-var makePromise = function (input) {
-  return driver.executeScript('return \'' + input + '\'').then(function (v) {
+var makePromise = function (input, amount) {
+  return driver.executeAsyncScript(function (aInput, aAmount) {
+    var done = arguments[arguments.length - 1];
+    window.setTimeout(function () {
+      done(aInput);
+    }, aAmount);
+  }, input, amount).then(function (v) {
     if (v !== input) return Promise.reject('Response out of sync. Expected "' + input + '", but was "' + v + '"');
     console.log('Retrieved: ' + v);
     return Promise.resolve(v);
   });
 };
 
-
 Promise.all([
-  makePromise('a'),
-  makePromise('b'),
-  makePromise('c'),
-  makePromise('d')
+  makePromise('\'a\'', 1000),
+  makePromise('\'b\'', 1200),
+  makePromise('\'c\'', 14000),
+  makePromise('\'d\'', 1000)
 ]).then(function () {
   console.log('Everything is good.');
 }, function (err) {
