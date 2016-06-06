405ea8e8762faa3eaefef794b592963cb0ae5145
morgan <morgan.smith@ephox.com>
2016-06-06 11:15:53 +1000

    TINY-491-deadlocks: HTML page testing until

--- test/js/node/WebdriverQueryTest.js
+++ test/js/node/WebdriverQueryTest.js
@@ -1,8 +1,14 @@
+var webdriver = require('selenium-webdriver');
+var By = webdriver.By;
+var until = webdriver.until;
+
 var driver = require('../../../src/js/bedrock/auto/driver').create({
   browser: 'firefox'
 });
 
 driver.manage().timeouts().setScriptTimeout(30000);
+
+driver.get('http://localhost/me/work/van/bedrock/test/js/node/page.html');
 
 var makePromise2 = function (aInput, aAmount) {
   return new Promise(function (resolve, reject) {
@@ -23,6 +29,34 @@ var makePromise2 = function (aInput, aAmount) {
     return Promise.resolve(v);
   });
 };
+
+var makeSyncPromise = function (input, amount) {
+  return driver.executeScript(function (aInput, aAmount) {
+    var start = new Date().getTime();
+    var finish = new Date().getTime();
+    return {
+      v: aInput,
+      delay: aAmount,
+      actualDelay: finish - start,
+      start: start,
+      finish: finish
+    };
+  }, input, amount).then(function (v) {
+    // if (v !== input) return Promise.reject('Response out of sync. Expected "' + input + '", but was "' + JSON.stringify(v) + '"');
+    console.log('Retrieved: ' + JSON.stringify(v));
+    return Promise.resolve(v);
+  });
+};
+
+var makeQuery = function (selector) {
+  return driver.wait(until.elementLocated(By.css(selector)), 1000).then(function (elem) {
+
+    console.log('found it: ' + selector);
+  }, function () {
+    console.log('did not find selector: ' + selector);
+  });
+};
+
 
 var makePromise = function (input, amount) {
   return driver.executeAsyncScript(function (aInput, aAmount) {
@@ -45,6 +79,16 @@ var makePromise = function (input, amount) {
   });
 };
 
+makePromise('a', 100);
+makeSyncPromise('a.sync');
+makeQuery('input'),
+makeQuery('div.progress'),
+makePromise('b', 1000);
+makePromise('c', 40);
+makeQuery('input');
+makeQuery('p');
+makeQuery('p');
+/*
 Promise.all([
   makePromise('\'a\'', 1000),
   makePromise('\'b\'', 50),
@@ -55,4 +99,5 @@ Promise.all([
 }, function (err) {
   console.error('error', err);
 });
+*/
 
