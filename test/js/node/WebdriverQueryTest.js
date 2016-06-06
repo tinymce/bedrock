b16fb5161b660f402f7d03ca9c5925782f2322c4
morgan <morgan.smith@ephox.com>
2016-06-06 10:52:46 +1000

    TINY-491-deadlocks: webdriver requests vs normal requests

--- test/js/node/WebdriverQueryTest.js
+++ test/js/node/WebdriverQueryTest.js
@@ -1,26 +1,54 @@
 var driver = require('../../../src/js/bedrock/auto/driver').create({
-  browser: 'chrome'
+  browser: 'firefox'
 });
 
 driver.manage().timeouts().setScriptTimeout(30000);
 
+var makePromise2 = function (aInput, aAmount) {
+  return new Promise(function (resolve, reject) {
+    var start = new Date().getTime();
+    setTimeout(function () {
+      var finish = new Date().getTime();
+      resolve({
+        v: aInput,
+        delay: aAmount,
+        actualDelay: finish - start,
+        start: start,
+        finish: finish
+      });
+    }, aAmount);
+  }).then(function (v) {
+    // if (v !== input) return Promise.reject('Response out of sync. Expected "' + input + '", but was "' + JSON.stringify(v) + '"');
+    console.log('Retrieved: ' + JSON.stringify(v));
+    return Promise.resolve(v);
+  });
+};
+
 var makePromise = function (input, amount) {
   return driver.executeAsyncScript(function (aInput, aAmount) {
+    var start = new Date().getTime();
     var done = arguments[arguments.length - 1];
     window.setTimeout(function () {
-      done(aInput);
+      var finish = new Date().getTime();
+      done({
+        v: aInput,
+        delay: aAmount,
+        actualDelay: finish - start,
+        start: start,
+        finish: finish
+      });
     }, aAmount);
   }, input, amount).then(function (v) {
-    if (v !== input) return Promise.reject('Response out of sync. Expected "' + input + '", but was "' + v + '"');
-    console.log('Retrieved: ' + v);
+    // if (v !== input) return Promise.reject('Response out of sync. Expected "' + input + '", but was "' + JSON.stringify(v) + '"');
+    console.log('Retrieved: ' + JSON.stringify(v));
     return Promise.resolve(v);
   });
 };
 
 Promise.all([
   makePromise('\'a\'', 1000),
-  makePromise('\'b\'', 1200),
-  makePromise('\'c\'', 14000),
+  makePromise('\'b\'', 50),
+  makePromise('\'c\'', 3000),
   makePromise('\'d\'', 1000)
 ]).then(function () {
   console.log('Everything is good.');
