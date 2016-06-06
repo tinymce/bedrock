a5bc2817057f38e3ed8828f6b39a1529a06672cb
morgan <morgan.smith@ephox.com>
2016-06-06 12:28:45 +1000

    TINY-491-deadlocks: TINY-491-deadlocks: assume there are bugs so just allow force locking

--- src/js/bedrock/server/drivermaster.js
+++ src/js/bedrock/server/drivermaster.js
@@ -15,15 +15,16 @@ var create = function () {
   // If the queue is empty and we are not "In Use", then this can lock.
 
 
-  var doWaitForIdle = function (identifier) {
+  var doWaitForIdle = function (identifier, f, label, attempts) {
+    if (attempts === 0) return use(f, label);
     if (inUse === false && queue.length === 0) return use(f, label);
     else if (inUse === false && queue[0].identifier === identifier) {
       var first = queue[0];
       queue = queue.slice(1);
       return use(first.f, first.label);
     } else {
-      return delay({}, 1000).then(function () {
-        return doWaitForIdle(identifier);
+      return delay({}, 100).then(function () {
+        return doWaitForIdle(identifier, f, label, attempts - 1);
       });
     }
   };
@@ -32,53 +33,28 @@ var create = function () {
   var waitForIdle = function (f, label) {
     if (inUse === false && queue.length === 0) return use(f, label);
     else {
-      var identifier = 's' + new Date().getTime() + Math.floor(Math.random() * 10000);
+      var identifier = label + '_' + new Date().getTime() + Math.floor(Math.random() * 10000);
       queue = queue.concat({
         f: f,
         label: label,
         identifier: identifier
       });
-      return doWaitForIdle(identifier);
+      return doWaitForIdle(identifier, f, label, 100);
     }
-    /*
-    console.log('ATTEMPTING LOCK:', label);
-    if (inUse === false && queue.length === 0) {
-      console.log('Allowing lock because queue empty and idle');
-      return use(f, label);
-    } else if (inUse === false && queue[0].label === label) {
-      console.log(label, 'This is the first thing in the queue, so allow lock');
-      queue = queue.slice(0);
-      return use(f, label);
-    } else {
-      console.log(label, 'Queueing ... not a priority');
-      if (! queue.find(function (q) {
-        return q.label === label;
-      })) {
-        queue = queue.concat({ f: f, label: label });
-      }
-      console.log(label, 'IN USE ... retrying in 1 second');
-      return delay({}, 1000).then(function () {
-
-        console.log('queue', queue);
-        return waitForIdle(f, label);
-      });
-    }
-    */
   };
 
 
   var use = function (f, label) {
     console.log('LOCKING', label);
-    var oldInUse = inUse;
     inUse = true;
 
     return f().then(function (v) {
-      console.log('UNLOCKING (SUCCESS)', label, oldInUse);
+      console.log('UNLOCKING (SUCCESS)', label);
       inUse = false;
       return v;
     }, function (err) {
       inUse = false;
-      console.log('UNLOCKING (FAILURE): ', err, label, oldInUse);
+      console.log('UNLOCKING (FAILURE): ', err, label);
       return err;
     });
   };
