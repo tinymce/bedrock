5cb0d2a7ea12b3009c5b2284345ff1d73f348bba
morgan <morgan.smith@ephox.com>
2016-06-09 13:49:16 +1000

    TINY-461: auto and manual mode

--- src/js/bedrock-cli.js
+++ src/js/bedrock-cli.js
@@ -1,6 +1,8 @@
 var run = function (program, directories) {
   var attempt = require('./bedrock/core/attempt');
-  var clis = require('./bedrock/cli/clis.js');
+  var clis = require('./bedrock/cli/clis');
+
+  if (clis[program.mode] === undefined)  throw 'Bedrock mode not known: ' + program.mode;
 
   var maybeSettings = clis[program.mode](directories);
   attempt.cata(maybeSettings, clis.log, program.go);
--- src/js/bedrock-manual.js
+++ src/js/bedrock-manual.js
@@ -1,33 +1,25 @@
-var run = function (directories) {
+var go = function (settings) {
   var serve = require('./bedrock/server/serve');
   var attempt = require('./bedrock/core/attempt');
 
-  var clis = require('./bedrock/cli/clis.js');
+  var serveSettings = {
+    projectdir: settings.projectdir,
+    basedir: settings.basedir,
+    config: settings.config,
+    testfiles: settings.testfiles,
+    // There is no driver for manual mode.
+    driver: attempt.failed('There is no webdriver for manual mode'),
+    master: attempt.failed('There is no master for manual mode'),
+    page: 'src/resources/bedrock.html'
+  };
 
-  var maybeSettings = clis.forManual(directories);
-
-  attempt.cata(maybeSettings, clis.log, function (settings) {
-
-    console.log('settings', settings);
-
-    var serveSettings = {
-      projectdir: settings.projectdir,
-      basedir: settings.basedir,
-      config: settings.config,
-      testfiles: settings.testfiles,
-      // There is no driver for manual mode.
-      driver: attempt.failed('There is no webdriver for manual mode'),
-      master: attempt.failed('There is no master for manual mode'),
-      page: 'src/resources/bedrock.html'
-    };
-
-    serve.start(serveSettings, function (service/* , done */) {
-      console.log('bedrock (manual) available at: http://localhost:' + service.port);
-    });
+  serve.start(serveSettings, function (service/* , done */) {
+    console.log('bedrock (manual) available at: http://localhost:' + service.port);
   });
 };
 
 module.exports = {
-  run: run
+  go: go,
+  mode: 'forManual'
 };
 
