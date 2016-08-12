a283829243fc966bd58348cb51bab2cdc12e2b64
morgan <morgan.smith@ephox.com>
2016-08-12 16:02:39 +1000

    VAN-17: Chrome options to disable extension popup

--- src/js/bedrock/auto/driver.js
+++ src/js/bedrock/auto/driver.js
@@ -4,10 +4,18 @@
  */
 var create = function (settings) {
   var webdriver = require('selenium-webdriver');
+  // Support for disabling the Automation Chrome Extension
+  var chrome = require('selenium-webdriver/chrome');
+  var chromeOptions = new chrome.Options();
 
-  return new webdriver.Builder()
-    .forBrowser(settings.browser)
+  chromeOptions.addArguments('chrome.switches', '--disable-extensions');
+
+  var driver = new webdriver.Builder()
+    .forBrowser(settings.browser).setChromeOptions(chromeOptions)
     .build();
+
+  driver.manage().window().maximize();
+  return driver;
 };
 
 module.exports = {
