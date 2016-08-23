1a07007aff90e8ba63a67070dda7681a2f6fbfb2
morgan <morgan.smith@ephox.com>
2016-08-24 07:28:44 +1000

    spike-repl: basic repl working!

--- src/js/bedrock/repl/replroutes.js
+++ src/js/bedrock/repl/replroutes.js
@@ -3,16 +3,30 @@ var generate = function (projectdir, basedir, boltConfig, replConfig) {
   var path = require('path');
   var routes = require('../server/routes');
 
+  var repl = path.relative(projectdir, replConfig);
+  var bc = path.relative(projectdir, boltConfig);
+  console.log('projectdir', projectdir);
+  console.log('repl', repl);
+  console.log('bc', bc);
+
+
+  var fs = require('fs');
+
+  var replFile = fs.readFileSync(repl);
+  var replContents = JSON.parse(replFile);
+
   // Duplication with boltroutes
   var routers = [
     routes.routing('GET', '/project', projectdir),
     routes.routing('GET', '/js', path.join(basedir, 'src/resources')),
-    routes.routing('GET', '/lib/bolt', path.join(basedir, 'node_modules/@ephox/bolt/lib')),
+
     routes.routing('GET', '/lib/jquery', path.join(basedir, 'node_modules/jquery/dist')),
+    routes.routing('GET', '/lib/bolt', path.join(basedir, 'node_modules/@ephox/bolt/lib')),
+    routes.rewrite('GET', projectdir, '/bingo', path.relative(projectdir, boltConfig)),
     // Very bolt specific.
     routes.json('GET', '/repl', {
       config: path.relative(projectdir, boltConfig),
-      repl: path.relative(projectdir, replConfig)
+      repl: replContents
     })
   ];
 
