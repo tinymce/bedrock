ac2db959b9906fddcaeb58fa2087afdcc5425fe2
morgan <morgan.smith@ephox.com>
2017-03-27 12:30:39 +1000

    VAN-70: Updating grunt file for sample

--- sample/Gruntfile.js
+++ sample/Gruntfile.js
@@ -6,13 +6,25 @@ module.exports = function(grunt) {
         projectdir: '../',
         files: {
           src: ['*Test.js']
-        }
+        },
+        testfiles: ['*PassTest.js']
+      }
+    },
+
+    'bedrock-auto': {
+      phantomjs: {
+        browser: 'phantomjs',
+        config: 'config.js',
+        projectdir: '../',
+        testfiles: ['*PassTest.js'],
+        name: 'phantomjs'
       }
     }
   });
 
   require("load-grunt-tasks")(grunt);
   grunt.loadTasks("../bin/grunt-tasks");
+  grunt.loadTasks("../tasks");
 
   grunt.registerTask("default", ["bedrock-manual"]);
 };
