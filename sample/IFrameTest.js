032203780ed2e50b5ca027a430afd5b38affcc35
morgan <morgan.smith@ephox.com>
2017-07-28 12:44:33 +1000

    iframe-support: Getting iframe test working on IE

--- sample/IFrameTest.js
+++ sample/IFrameTest.js
@@ -11,40 +11,50 @@ asynctest(
 
     var iframe = document.createElement('iframe');
     iframe.setAttribute('class', 'iframe-class');
-    iframe.addEventListener('load', function () {
-      var doc = iframe.contentWindow.document;
-      doc.open();
-      doc.writeln('<! doctype><html><body contenteditable="true">Body of content editable</body></html>');
-      doc.close();
 
-      iframe.contentWindow.document.body.focus();
+    var frameLoader = function () {
+      iframe.removeEventListener('load', frameLoader);
+      console.log('iframe loading');
+      try {
+        var doc = iframe.contentWindow.document;
+        doc.open('text/html', 'replace');
+        doc.writeln('<! doctype><html><body contenteditable="true">Body of content editable</body></html>');
+        doc.close();
 
-      sendText('iframe=>body', 'going').then(function () {
-        sendText('textarea', 'blah');
-      }).then(function () {
-        success();
+        iframe.contentWindow.document.body.focus();
+      } catch (err) {
+        console.error(err);
+      }
+
+      sendText('iframe=>body', 'going', function () {
+        sendText('textarea', 'blah', function () {
+          success();
+        }, failure);
       }, failure);
-    });
-
-
-    var sendText = function (selector, text) {
-      return new Promise(function (resolve, reject) {
-        var request = new XMLHttpRequest();
-        request.open('POST', '/keys', true);
-        request.onload = function () {
-          resolve();
-        };
-
-        request.onerror = function () {
-          reject();
-        };
-
-        request.send(JSON.stringify({
-          selector: selector,
-          keys: [{ text: text }]
-        }));
-      });
     };
+
+    iframe.addEventListener('load', frameLoader);
+
+
+    var sendText = function (selector, text, onSuccess, onFailure) {
+      console.log('sending text');
+      var request = new XMLHttpRequest();
+      request.open('POST', '/keys', true);
+      request.onload = function () {
+        onSuccess();
+      };
+
+      request.onerror = function () {
+        onFailure();
+      };
+
+      request.send(JSON.stringify({
+        selector: selector,
+        keys: [{ text: text }]
+      }));
+    };
+
+    console.log('page is loading');
 
     setTimeout(function () {
 
