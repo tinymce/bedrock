15dd63780a59e2cec89859b09787364d652b2db8
Mike Pheasant <mike.pheasant@ephox.com>
2017-08-03 13:30:59 +1000

    sauce-uploads: bolt and jquery resolution

--- test/atomic/projectuploads-test.js
+++ test/atomic/projectuploads-test.js
@@ -1,17 +1,15 @@
 var uploads = require('../../src/js/bedrock/remote/project-uploads');
 var tape = require('tape');
 
-
-
-
 tape('Check uploading', function (t) {
   var actual = uploads.choose(
     {
       name: 'random.name',
       bucket: 'bedrock-testing',
       uploaddirs: [ 'test/resources/alpha', 'test/resources/beta' ],
-      basedir: '${BASE}',
-      projectdir:  '${PROJECT}',
+      dependencydir: "node_modules",
+      basedir: '.',
+      projectdir: '${PROJECT}',
       config: 'config/sample.js',
       testfiles: [
         'file1'
@@ -21,16 +19,16 @@ tape('Check uploading', function (t) {
   t.deepEqual(actual, [
       { isDir: true, source: '${PROJECT}/test/resources/alpha', destination: 'project/test/resources/alpha' },
       { isDir: true, source: '${PROJECT}/test/resources/beta', destination: 'project/test/resources/beta' },
-      { isFile: true, source: '${BASE}/src/resources/runner.js', destination: 'js/runner.js' },
-      { isFile: true, source: '${BASE}/node_modules/@ephox/bolt/lib/kernel.js', destination: 'lib/bolt/kernel.js' },
-      { isFile: true, source: '${BASE}/node_modules/@ephox/bolt/lib/loader.js', destination: 'lib/bolt/loader.js' },
-      { isFile: true, source: '${BASE}/node_modules/@ephox/bolt/lib/module.js', destination: 'lib/bolt/module.js' },
-      { isFile: true, source: '${BASE}/node_modules/@ephox/bolt/lib/test.js', destination: 'lib/bolt/test.js' },
-      { isFile: true, source: '${BASE}/node_modules/jquery/dist/jquery.min.js', destination: 'lib/jquery/jquery.min.js' },
-      { isFile: true, source: '${BASE}/src/resources/bedrock.html', destination: 'index.html' },
-      { isFile: true, source: '${BASE}/src/css/bedrock.css', destination: 'css/bedrock.css' },
+      { isFile: true, source: 'src/resources/runner.js', destination: 'js/runner.js' },
+      { isFile: true, source: 'node_modules/@ephox/bolt/lib/kernel.js', destination: 'lib/bolt/kernel.js' },
+      { isFile: true, source: 'node_modules/@ephox/bolt/lib/loader.js', destination: 'lib/bolt/loader.js' },
+      { isFile: true, source: 'node_modules/@ephox/bolt/lib/module.js', destination: 'lib/bolt/module.js' },
+      { isFile: true, source: 'node_modules/@ephox/bolt/lib/test.js', destination: 'lib/bolt/test.js' },
+      { isFile: true, source: 'node_modules/jquery/dist/jquery.min.js', destination: 'lib/jquery/jquery.min.js' },
+      { isFile: true, source: 'src/resources/bedrock.html', destination: 'index.html' },
+      { isFile: true, source: 'src/css/bedrock.css', destination: 'css/bedrock.css' },
       { content: '{"config":"config/sample.js","scripts":["file1"]}', contentType: undefined, filename: 'harness' }
-    ]
+  ]
   );
   t.end();
 });
