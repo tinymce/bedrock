07e973d349bf69f024fd0399218db2258f088206
Andrew Herron <thespyder@programmer.net>
2017-12-20 15:13:42 +1000

    Escape slashes properly on the filePath variable as well; that's used for UI and JSON output. Just made them forwarded slashes.

--- src/js/bedrock/compiler/imports.js
+++ src/js/bedrock/compiler/imports.js
@@ -8,6 +8,11 @@ let filePathToImport = function (useRequire, scratchFile) {
     // make sure slashes are escaped for windows
     relativePath = relativePath.replace(/\\/g, '\\\\');
 
+    // make sure backslashes are replaced with forward slash for the UI and JSON output string.
+    // Escaping the slashes would also work, but in case we accidentally interpret them later
+    // let's just go with forward slash.
+    filePath = filePath.replace(/\\/g, '/');
+
     return [
       useRequire ? 'require("' + relativePath + '");' : 'import "' + relativePath + '";', // rollup doesn't support require
       'if (__tests && __tests[__tests.length - 1] && !__tests[__tests.length - 1].filePath) {',
