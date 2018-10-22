cf6bfd053f27bfa4950ae45e275b5b6006483de4
morgan <morgan.smith@ephox.com>
2018-10-22 12:33:29 +1000

    logbook-history: Only process logs if present

--- src/client/ts/api/UnitTest.ts
+++ src/client/ts/api/UnitTest.ts
@@ -17,41 +17,47 @@ var register = function (name, test) {
   Global.__tests.push({ name: name, test: test });
 };
 
+
+const processLog = (err, logs) => {
+  const outputToStr = function (indent, entries) {
+    let everything = [ ];
+
+    for (let i = 0; i < entries.length; i++) {
+      const entry = entries[i];
+      const output = (function () {
+        const traceLines = entry.trace === null ? [ ] : [ '', '', '' ].concat(entry.trace.split('\n'));
+
+        if (entry.entries.length === 0) {
+          if (entry.trace === null) {
+            return [ indent + '*  ' + entry.message ];
+          } else {
+            return [ indent + '*  ' + entry.message ].concat(traceLines);
+          }
+        } else {
+          // We have entries ... let's format them.
+          return [ indent + '*  ' + entry.message ].concat(
+            outputToStr(indent + '  ', entry.entries)
+          ).concat(traceLines);
+        }
+      })();
+      everything = everything.concat(output)
+    }
+    return everything;
+  };
+
+  const processed = outputToStr('  ', logs.history);
+
+  return JSON.stringify({
+    error: err instanceof Error ? err.message : err,
+    logs: processed
+  }, null, 2);
+}
+
 var asynctest = function (name: string, test: (success: SuccessCallback, failure: FailureCallback) => void) {
   register(name, function (success, failure) {
     test(success, function (err, logs) {
-      const outputToStr = function (indent, entries) {
-        let everything = [ ];
-
-        for (let i = 0; i < entries.length; i++) {
-          const entry = entries[i];
-          const output = (function () {
-            const traceLines = entry.trace === null ? [ ] : [ '', '', '' ].concat(entry.trace.split('\n'));
-
-            if (entry.entries.length === 0) {
-              if (entry.trace === null) {
-                return [ indent + '*  ' + entry.message ];
-              } else {
-                return [ indent + '*  ' + entry.message ].concat(traceLines);
-              }
-            } else {
-              // We have entries ... let's format them.
-              return [ indent + '*  ' + entry.message ].concat(
-                outputToStr(indent + '  ', entry.entries)
-              ).concat(traceLines);
-            }
-          })();
-          everything = everything.concat(output)
-        }
-        return everything;
-      };
-
-      const processed = outputToStr('  ', logs.history);
-
-      failure(JSON.stringify({
-        error: err instanceof Error ? err.message : err,
-        logs: processed
-      }, null, 2));
+      const failureMessage = logs !== undefined ? processLog(err, logs) : err;
+      failure(failureMessage);
     });
   });
 };
