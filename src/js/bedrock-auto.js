c6b82fecd6603e5de27ba5e48a455f3c0e3c1023
Lee Newson <lee.newson@tiny.cloud>
2019-03-28 12:20:47 +1000

    Improvement to exit out earlier before attempting to compile

--- src/js/bedrock-auto.js
+++ src/js/bedrock-auto.js
@@ -23,6 +23,14 @@ var go = function (settings) {
 
   var reporter = require('./bedrock/core/reporter');
 
+  // If the browser is Safari, then we need to skip the tests because in v12.1 they removed the
+  // --legacy flag in safaridriver that was required to run webdriver
+  if (settings.browser === 'safari') {
+    console.warn('Skipping tests as webdriver is currently broken on Safari');
+    skipTests(reporter, settings);
+    return;
+  }
+
   var master = require('./bedrock/server/drivermaster').create();
   var driver = require('./bedrock/auto/driver');
 
@@ -35,61 +43,54 @@ var go = function (settings) {
 
   console.log('bedrock-auto ' + version + ' starting...');
 
-  // If the browser is Safari, then we need to skip the tests because in v12.1 they removed the
-  // --legacy flag in safaridriver that was required to run webdriver
-  if (settings.browser === 'safari') {
-    console.warn('Skipping tests as webdriver is currently broken on Safari');
-    skipTests(reporter, settings);
-  } else {
-    routes.then((runner) => {
-      driver.create({
-        browser: settings.browser,
+  routes.then((runner) => {
+    driver.create({
+      browser: settings.browser,
+      basedir: settings.basedir,
+      debuggingPort: settings.debuggingPort
+    }).then(function (driver) {
+      var serveSettings = {
+        projectdir: settings.projectdir,
         basedir: settings.basedir,
-        debuggingPort: settings.debuggingPort
-      }).then(function (driver) {
-        var serveSettings = {
-          projectdir: settings.projectdir,
-          basedir: settings.basedir,
-          testfiles: settings.testfiles,
-          driver: attempt.passed(driver),
-          master: master,
-          runner: runner,
-          loglevel: settings.loglevel,
-          customRoutes: settings.customRoutes,
-          stickyFirstSession: true,
-          overallTimeout: settings.overallTimeout,
-          singleTimeout: settings.singleTimeout
-        };
+        testfiles: settings.testfiles,
+        driver: attempt.passed(driver),
+        master: master,
+        runner: runner,
+        loglevel: settings.loglevel,
+        customRoutes: settings.customRoutes,
+        stickyFirstSession: true,
+        overallTimeout: settings.overallTimeout,
+        singleTimeout: settings.singleTimeout
+      };
 
-        serve.start(serveSettings, function (service, done) {
-          if (!isPhantom) console.log('bedrock-auto ' + version + ' available at: http://localhost:' + service.port);
-          var result = driver.get('http://localhost:' + service.port)
-            .then(driver.executeScript('window.focus();'))
-            .then(function () {
-              var message = isPhantom ? '\nPhantom tests loading ...\n' : '\nInitial page has loaded ...\n';
-              console.log(message);
-              service.markLoaded();
-              service.enableHud();
-              return service.awaitDone().then(function (data) {
-                return reporter.write({
-                  name: settings.name,
-                  output: settings.output
-                })(data);
-              }, function (pollExit) {
-                return reporter.writePollExit({
-                  name: settings.name,
-                  output: settings.output
-                }, pollExit);
-              });
-            });
-
-          lifecycle.shutdown(result, driver, done, settings.gruntDone !== undefined ? settings.gruntDone : null, settings.delayExit !== undefined ? settings.delayExit : false);
+      serve.start(serveSettings, function (service, done) {
+        if (!isPhantom) console.log('bedrock-auto ' + version + ' available at: http://localhost:' + service.port);
+        var result = driver.get('http://localhost:' + service.port)
+                           .then(driver.executeScript('window.focus();'))
+                           .then(function () {
+          var message = isPhantom ? '\nPhantom tests loading ...\n' : '\nInitial page has loaded ...\n';
+          console.log(message);
+          service.markLoaded();
+          service.enableHud();
+          return service.awaitDone().then(function (data) {
+            return reporter.write({
+              name: settings.name,
+              output: settings.output
+            })(data);
+          }, function (pollExit) {
+            return reporter.writePollExit({
+              name: settings.name,
+              output: settings.output
+            }, pollExit);
+          });
         });
-      }, function (err) {
-        console.error('Unable to create driver', err);
+
+        lifecycle.shutdown(result, driver, done, settings.gruntDone !== undefined ? settings.gruntDone : null, settings.delayExit !== undefined ? settings.delayExit : false);
       });
+    }, function (err) {
+      console.error('Unable to create driver', err);
     });
-  }
+  });
 
 };
 
