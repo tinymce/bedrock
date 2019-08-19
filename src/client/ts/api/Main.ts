cab701242bd516ae55d7e1845f0114ad84e8ce80
Dylan Just <dylan.just@ephox.com>
2019-08-19 11:56:58 +1000

    Rename back for backwards compatibility.

--- src/client/ts/api/Main.ts
+++ src/client/ts/api/Main.ts
@@ -2,4 +2,4 @@ import * as Assert from './Assert';
 import * as UnitTest from './UnitTest';
 import { TestLabel } from './TestLabel';
 
-export { Assert, UnitTest, TestLabel };
+export { Assert as assert, UnitTest, TestLabel };
