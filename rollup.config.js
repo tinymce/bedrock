88f3ace7017026f6f293c6a1658cfadd38622ce3
Dylan Just <dylan.just@ephox.com>
2019-08-28 17:31:01 +1000

    Draft ts conversion of runner.

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,17 @@
-import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-  input: 'src/runner/ts/Runner.ts',
+  input: 'lib/runner/ts/Runner.js',
   output: {
     file: 'dist/runner/runner.js',
-    format: 'iife'
+    format: 'iife',
+    name: 'runner',
+    globals: {
+      'jquery': 'jquery'
+    },
   },
+  context: 'window',
   plugins: [
-    typescript({
-      "target": "es5",
-      "module": "es2015",
-      "lib": ["es2015", "dom"],
-      include: "src/runner/ts"
-    })
+    resolve()
   ]
 };
