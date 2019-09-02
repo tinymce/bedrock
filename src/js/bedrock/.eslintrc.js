24bed2b51fe9dd2623c4089cbbb4b0d92fb60638
Dylan Just <dylan.just@ephox.com>
2019-09-02 11:36:40 +1000

    Eslint settings for main app.

---
+++ src/js/bedrock/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+  },
+  'rules': {
+    "max-len": "off",
+    "space-before-function-paren": ["error", "always"],
+    "comma-dangle": ["error", "never"]
+  },
+};
