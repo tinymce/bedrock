9c3f18da65dd172f984d6f06ffa675fd421bea12
Dylan Just <dylan.just@ephox.com>
2019-10-02 12:11:18 +1000

    Split Runner.clean out as HtmlReporter.report

--- modules/runner/src/main/ts/Runner.ts
+++ modules/runner/src/main/ts/Runner.ts
@@ -1,13 +1,9 @@
 import { UrlParams } from './core/UrlParams';
 import {
-  AssertionError,
-  HtmlDiffAssertionError,
   LoggedError,
-  NormalizedTestError,
-  PprintAssertionError
 } from './alien/ErrorTypes';
 import { HarnessResponse } from './core/ServerTypes';
-import * as Differ from './core/Differ';
+import * as HtmlReporter from './reporter/HtmlReporter';
 
 declare const $: JQueryStatic;
 
@@ -90,76 +86,6 @@ const sendDone = (session: string, onSuccess: onSuccessCallback, onError: onErro
     session: session,
     coverage: getCoverage(),
   }, onSuccess, onError);
-};
-
-const htmlentities = (str: string): string =>
-  String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-
-/* Required to make <del> and <ins> stay as tags.*/
-const processQUnit = (html: string): string =>
-  (html
-    .replace(/&lt;del&gt;/g, '<del>')
-    .replace(/&lt;\/del&gt;/g, '</del>')
-    .replace(/&lt;ins&gt;/g, '<ins>')
-    .replace(/&lt;\/ins&gt;/g, '</ins>'));
-
-const isPprintAssertionError = (err: NormalizedTestError): err is PprintAssertionError => {
-  return err.name === 'PprintAssertionError';
-};
-
-const isHTMLDiffError = (err: NormalizedTestError): err is HtmlDiffAssertionError => {
-  return err.name === 'HtmlAssertion';
-};
-
-const isAssertionError = (err: NormalizedTestError): err is AssertionError => {
-  return err.name === 'AssertionError';
-};
-
-const formatExtra = (e: LoggedError): string => {
-  if (!e.logs) {
-    if (e.error && e.error.stack) {
-      const lines = e.error.stack.split('\n').filter((line) =>
-        line.indexOf('at') !== -1);
-      return '\n\nStack:\n' + lines.join('\n');
-    } else {
-      return '';
-    }
-  } else {
-    const lines = e.logs.map((log) =>
-      log.replace(/\n/g, '\\n').replace(/\r/g, '\\r'));
-    return '\n\nLogs:\n' + lines.join('\n');
-  }
-};
-
-const clean = (err: LoggedError): string => {
-  const e = err === undefined ? new Error('no error given') : err.error;
-
-  const extra = formatExtra(err);
-  if (isHTMLDiffError(e)) {
-    // Provide detailed HTML comparison information
-    return 'Test failure: ' + e.message +
-      '\nExpected: ' + htmlentities(e.diff.expected) +
-      '\nActual: ' + htmlentities(e.diff.actual) +
-      '\n\nHTML Diff: ' + processQUnit(htmlentities(e.diff.comparison)) +
-      extra;
-  } else if (isPprintAssertionError(e)) {
-    const d = Differ.diffLineMode(e.diff.expected, e.diff.actual);
-    const dh = Differ.diffPrettyHtml(d);
-    return 'Test failure: ' + e.message +
-      '\nExpected: \n' + htmlentities(e.diff.expected) +
-      '\nActual: \n' + htmlentities(e.diff.actual) +
-      '\nDiff: \n' + dh;
-  } else if (isAssertionError(e)) {
-    return 'Assertion error' + (e.message ? ' [' + e.message + ']' : '') +
-      ': [' + htmlentities(JSON.stringify(e.expected)) + '] ' + e.operator +
-      ' [' + htmlentities(JSON.stringify(e.actual)) + ']' + extra;
-  } else if (e.name && e.message) {
-    return htmlentities(e.name + ': ' + e.message + extra);
-  } else if (e.toString !== undefined) {
-    return htmlentities(String(e) + extra);
-  } else {
-    return htmlentities(JSON.stringify(e) + extra);
-  }
 };
 
 let passCount = 0;
@@ -274,7 +200,7 @@ const reporter = (() => {
       marker.text('[failed]').addClass('failed');
       // Don't use .text() as it strips out newlines in IE, even when used
       // on a pre tag.
-      const errorMessage = clean(e);
+      const errorMessage = HtmlReporter.report(e);
       const pre = $('<pre/>')
         .addClass('error')
         .html(errorMessage);
