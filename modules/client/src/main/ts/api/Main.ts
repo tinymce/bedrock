1d5191f3d277c2eecfa6b6936de8486265bcc82f
Dylan Just <dylan.just@ephox.com>
2019-10-02 16:21:41 +1000

    Import consistency.

--- modules/client/src/main/ts/api/Main.ts
+++ modules/client/src/main/ts/api/Main.ts
@@ -1,6 +1,6 @@
 import * as LegacyAssert from './LegacyAssert';
-import * as Assert from './NewAssert';
+import * as NewAssert from './NewAssert';
 import * as UnitTest from './UnitTest';
 import { TestLabel } from './TestLabel';
 
-export { LegacyAssert as assert, Assert, UnitTest, TestLabel };
+export { LegacyAssert as assert, NewAssert as Assert, UnitTest, TestLabel };
