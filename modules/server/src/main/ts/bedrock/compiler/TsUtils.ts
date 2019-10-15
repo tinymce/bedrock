1e9039f90eac32cd27875a1b8977448379945836
Spocke <spocke@moxiecode.com>
2019-10-15 09:10:36 +0200

    TINY-4243: fixed missing escape for dot in tsx file check

--- modules/server/src/main/ts/bedrock/compiler/TsUtils.ts
+++ modules/server/src/main/ts/bedrock/compiler/TsUtils.ts
@@ -1,4 +1,4 @@
-const hasTs = (srcFiles: string[]) => srcFiles.findIndex((fileName) => /(.tsx?)$/.test(fileName)) !== -1;
+const hasTs = (srcFiles: string[]) => srcFiles.findIndex((fileName) => /(\.tsx?)$/.test(fileName)) !== -1;
 
 export {
   hasTs
