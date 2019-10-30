e5ab560e5031fe151b3a069043a769e20a9b9796
Dylan Just <dylan.just@ephox.com>
2019-10-30 17:39:52 +1000

    Remove tests for node assertions. Their behaviour is different on different node platforms, so this test is fragile. It's not worth keeping for the value it adds at this stage.

--- modules/common/src/test/ts/api/ReporterTest.ts
+++ modules/common/src/test/ts/api/ReporterTest.ts
@@ -17,26 +17,6 @@ function htmlAssertion() {
 }
 
 describe("Reporter", () => {
-  it('Reports Node Assertions as text', () => {
-    try {
-      NodeAssert.strictEqual(3, 4, 'blah');
-    } catch (e) {
-      const actual = Reporter.text(LoggedError.loggedError(e, []));
-      const expected = 'Assertion error [blah]: [4] strictEqual [3]\n\nLogs:\n';
-      assert.deepEqual(actual, expected, 'text output');
-    }
-  });
-
-  it('Reports Node Assertions as html', () => {
-    try {
-      NodeAssert.strictEqual('<span>', '<div>', 'blah');
-    } catch (e) {
-      const actual = Reporter.html(LoggedError.loggedError(e, []));
-      const expected = 'Assertion error [blah]: [&quot;&lt;div&gt;&quot;] strictEqual [&quot;&lt;span&gt;&quot;]\n\nLogs:\n';
-      assert.deepEqual(actual, expected, 'text output');
-    }
-  });
-
   it('Reports thrown js errors as html', () => {
     try {
       // noinspection ExceptionCaughtLocallyJS
