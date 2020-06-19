2cd08afb97678b32c859ddea66104a10757dd127
Dylan Just <dylan.just@tiny.cloud>
2020-06-19 22:00:54 +1000

    Remove the "suite" tests.
    The failing tests were timing out instead of failing properly. We don't need them.

--- modules/sample/src/test/ts/suite/Assert.ts
+++
@@ -1,9 +0,0 @@
-let eq = function (a: any, b: any, label?: string) {
-    if (a !== b) {
-        throw new Error('a is not b');
-    }
-};
-
-export {
-    eq
-};
--- modules/sample/src/test/ts/suite/SyncPassTest.ts
+++
@@ -1,6 +0,0 @@
-import * as Suite from './Suite';
-import * as Assert from './Assert';
-
-Suite.test('SyncPassTest ts', function () {
-  Assert.eq(1, 1);
-});
