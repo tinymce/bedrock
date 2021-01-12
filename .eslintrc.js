2e0184e1b6aa863c5d46f2378a805655ef83d701
techtangents <167879+techtangents@users.noreply.github.com>
2021-01-12 17:10:50 +1000

    Remove unnecessary rule override.

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,6 @@ module.exports = {
     }],
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/explicit-module-boundary-types': [ 'error', { 'allowArgumentsExplicitlyTypedAsAny': true } ],
-    '@typescript-eslint/no-inferrable-types': 'off',
 
     'arrow-spacing': 'error',
     'no-multi-spaces': [ 'error', { ignoreEOLComments: true }],
