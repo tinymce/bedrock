aa3685e44bc923888155b4668960bf0a9cadcc88
Lee Newson <lee.newson@tiny.cloud>
2021-12-29 09:12:41 +1000

    TINY-7347: Added the ability to serve binary files from custom routes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## 13.0.0 - 2022-01-10
 
+### Added
+- Added the ability to serve binary files from custom routes.
+
 ### Improved
 - Upgrade to `webpack-dev-server` 4.x.
 
---
+++ modules/sample/src/test/ts/client/pass/CustomRouteTest.ts
@@ -0,0 +1,38 @@
+import { Assert, describe, it } from '@ephox/bedrock-client';
+
+describe('CustomRouteTest', () => {
+  const sendTest = (url: string, responseType: XMLHttpRequestResponseType, assert: (xhr: XMLHttpRequest) => void) => (done: (err?: any) => void) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', () => {
+      try {
+        assert(xhr);
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+
+    xhr.open('GET', url);
+    xhr.responseType = responseType;
+    xhr.send();
+  };
+
+  it('undefined route', sendTest('/custom/non-existent', 'json', (xhr) => {
+    Assert.eq('Status', 404, xhr.status);
+    Assert.eq('Content Type', 'text/plain', xhr.getResponseHeader('Content-Type'));
+  }));
+
+  it('image binary file route', sendTest('/custom/image', 'arraybuffer', (xhr) => {
+    Assert.eq('Status', 200, xhr.status);
+    Assert.eq('Content Type', 'image/jpeg', xhr.getResponseHeader('Content-Type'));
+    Assert.eq('Content Length', '95986', xhr.getResponseHeader('Content-Length'));
+  }));
+
+  it('text binary file route', sendTest('/custom/text', 'text', (xhr) => {
+    Assert.eq('Status', 200, xhr.status);
+    Assert.eq('Content Type', 'text/plain; charset=utf-8', xhr.getResponseHeader('Content-Type'));
+    Assert.eq('Content Length', '123', xhr.getResponseHeader('Content-Length'));
+    Assert.eq('Content', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', xhr.response);
+  }));
+});
