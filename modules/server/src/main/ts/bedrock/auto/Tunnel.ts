b237659703921e8218ad3c19d46d2e1b2c6ba899
Ky Eltis <skylite.eltis@gmail.com>
2023-07-19 14:43:09 +1000

    sish tunnel

--- modules/server/src/main/ts/bedrock/auto/Tunnel.ts
+++ modules/server/src/main/ts/bedrock/auto/Tunnel.ts
@@ -8,40 +8,44 @@ export interface Tunnel {
     shutdown: () => Promise<void>;
 }
 
-const load = (): ExecUtils.ChildAPI => {
+const load = (subdomain: string, port: number): ExecUtils.ChildAPI => {
     // Installs tunnelmole in current node project. If testing on TinyMCE it will install in TinyMCE package.json.
-    const exec = 'npx';
-    const args = [ '--yes', 'tunnelmole'];
+    const exec = 'ssh';
+    const args = ['-R', subdomain + ':80:localhost:' + port, 'sish.osu.tiny.work'];
     return ExecUtils.execLoader(exec, args);
 };
 
-const waitForUrl = (proc: ChildProcess, timeout = 20000): Promise<string> => {
+const waitForReady = (proc: ChildProcess, timeout = 20000): Promise<void> => {
     return new Promise((resolve, reject) => {
-        setTimeout(() => reject('Tunnel could not be installed with npx.'), timeout);
+        setTimeout(() => reject('Tunnel creation took too long.'), timeout);
         const output = (proc.stdout ?? reject()) as internal.Readable;
-        let url;
-        const wordStream = output.pipe(split2(' '));
+
+        const wordStream = output.pipe(split2());
         wordStream.on('data', (data) => {
-            url = data.toString();
             console.log('Tunnel says:', data.toString());
             // Stop listening. Even though it resolves instantly, this code block would otherwise
             // still be run on every data event.
-            wordStream.removeAllListeners('data');
-            resolve(url);
+            // wordStream.removeAllListeners('data');
+            resolve();
         });
         proc.stderr?.on('data', (data) => {
             console.error('Tunnel error:', data.toString());
+        });
+        proc.on('close', (data) => {
+            console.log('Tunnel closed', data);
         });
     });
 };
 
 export const create = async (port: number): Promise<Tunnel> => {
-    const api = load();
-    const tunnelProc = api.start([port.toString()]);
+    const subdomain = crypto.randomUUID();
+    const api = load(subdomain, port);
+    const tunnelProc = api.start();
     console.log('Tunnel process running. Waiting for URL...');
 
     try {
-        const urlText = await waitForUrl(tunnelProc);
+        await waitForReady(tunnelProc);
+        const urlText = 'http://' + subdomain + '.sish.osu.tiny.work';
         console.log('Tunnel URL: ' + urlText);
         const url = new URL(urlText);
 
