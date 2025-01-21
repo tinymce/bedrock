b84f05a5b4965586f9de554b1c1bde0a41947a1e
Andrew Herron <thespyder@programmer.net>
2025-01-21 13:06:10 +1100

    TINY-11177: Add describe block wrappers to old style tests to more easily identify them

--- modules/client/src/main/ts/api/UnitTest.ts
+++ modules/client/src/main/ts/api/UnitTest.ts
@@ -1,5 +1,5 @@
 import { Context, Failure, TestError, TestLabel, TestLogs } from '@ephox/bedrock-common';
-import { it } from './Bdd';
+import { describe, it } from './Bdd';
 
 type TestLogs = TestLogs.TestLogs;
 type TestError = TestError.TestError;
@@ -10,11 +10,13 @@ export type FailureCallback = (error: TestThrowable, logs?: TestLogs) => void;
 
 /** An asynchronous test with callbacks. */
 export const asyncTest = (name: string, test: (this: Context, success: SuccessCallback, failure: FailureCallback) => void): void => {
-  it(name, function (done) {
-    test.call(this, () => done(), ((err, logs) => {
-      const r = Failure.prepFailure(err, logs);
-      done(r);
-    }));
+  describe('old-style test', () => {
+    it(name, function (done) {
+      test.call(this, () => done(), ((err, logs) => {
+        const r = Failure.prepFailure(err, logs);
+        done(r);
+      }));
+    });
   });
 };
 
@@ -23,10 +25,14 @@ export const asynctest = asyncTest;
 
 /** A synchronous test that fails if an exception is thrown */
 export const test = (name: string, test: (this: Context) => void): void => {
-  it(name, test);
+  describe('old-style test', () => {
+    it(name, test);
+  });
 };
 
 /** Tests an async function (function that returns a Promise). */
 export const promiseTest = (name: string, test: (this: Context) => Promise<void>): void => {
-  it(name, test);
+  describe('old-style test', () => {
+    it(name, test);
+  });
 };
